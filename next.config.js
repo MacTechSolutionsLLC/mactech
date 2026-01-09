511db92e9e7409162c5625707c962a7bc2266f26
Patrick <patcaru@icloud.com>
2026-01-08 22:31:53 -0800

    Exclude vetted folder from TypeScript compilation and Next.js build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Exclude vetted folder from build
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  webpack: (config, { isServer }) => {
+    // Exclude vetted folder from webpack compilation
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/node_modules/**', '**/vetted/**'],
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
