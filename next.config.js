a5b00c7e1e064d61cb7075c43b0f20406c023ad6
Patrick <patcaru@icloud.com>
2026-01-08 22:32:40 -0800

    Simplify webpack config - TypeScript exclusion should be sufficient

--- next.config.js
+++ next.config.js
@@ -1,28 +1,12 @@
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Exclude vetted folder from build
-  webpack: (config, { isServer }) => {
-    // Exclude vetted folder from webpack compilation
-    config.module = config.module || {}
-    config.module.rules = config.module.rules || []
-    
-    // Add rule to ignore vetted folder
-    config.module.rules.push({
-      test: /\.(ts|tsx|js|jsx)$/,
-      include: (filePath) => {
-        return !filePath.includes(path.join(process.cwd(), 'vetted'))
-      },
-    })
-    
-    // Ignore vetted folder in webpack watch
+  // Exclude vetted folder from webpack watch (for dev mode)
+  webpack: (config) => {
     config.watchOptions = {
       ...config.watchOptions,
       ignored: ['**/node_modules/**', '**/vetted/**'],
     }
-    
     return config
   },
 }
