82855dbd376db911b67b2dc54d97709533cbc6f0
Patrick <patcaru@icloud.com>
2026-01-08 22:32:29 -0800

    Add webpack rule to exclude vetted folder from compilation

--- next.config.js
+++ next.config.js
@@ -1,24 +1,29 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   // Exclude vetted folder from build
   webpack: (config, { isServer }) => {
     // Exclude vetted folder from webpack compilation
-    if (config.resolve) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-      }
-    }
-    // Ignore vetted folder in webpack
+    config.module = config.module || {}
+    config.module.rules = config.module.rules || []
+    
+    // Add rule to ignore vetted folder
+    config.module.rules.push({
+      test: /\.(ts|tsx|js|jsx)$/,
+      include: (filePath) => {
+        return !filePath.includes(path.join(process.cwd(), 'vetted'))
+      },
+    })
+    
+    // Ignore vetted folder in webpack watch
     config.watchOptions = {
       ...config.watchOptions,
       ignored: ['**/node_modules/**', '**/vetted/**'],
     }
+    
     return config
-  },
-  // Exclude vetted from page extensions discovery
-  experimental: {
-    // This ensures Next.js only looks in the root app directory
   },
 }
 
